15ebc2fd699532bc35461d1986669c199c5a04ad
Venkat Bijjam <venkatbijjam@Venkats-MacBook-Pro.local>
2023-06-21 22:36:59 -0400

    MG-005 added semantic navigation

---
+++ src/challenges/Navigation.js
@@ -0,0 +1,32 @@
+const Navigation = () => {
+return (
+    <>
+    <header>
+        <a href="#" >
+        <img src="https://assets.codepen.io/296057/lil-bv-logo.svg" alt="Binaryville. Click for home." />
+        </a>
+        <nav>
+            <ul>
+                <li>
+                <a href="#" >Home</a>
+                </li>
+                <li>
+                <a href="#" >About</a>
+                </li>
+                <li>
+                <a href="#" >Products</a>
+                </li>
+                <li>
+                <a href="#" >Services</a>
+                </li>
+                <li>
+                <a href="#" >Contact</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    </>
+)
+}
+
+export default Navigation;
